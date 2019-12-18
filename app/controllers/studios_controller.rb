1e423075c5ea6d33dcbc9fd457027eaaa9f6d4cf
yuya-yamaguchi <56626222+yuya-yamaguchi@users.noreply.github.com>
2019-12-18 13:18:47 +0900

    スタジオ詳細・予約表の作成 (#4)
    * 初期gemfileの導入・haml変換 (#1)
    
    * create reserve_DB and create seed
    
    * create studio details info
    
    * create reserve list

---
+++ app/controllers/studios_controller.rb
@@ -0,0 +1,11 @@
+class StudiosController < ApplicationController
+
+  def show
+    @studio = Studio.find(params[:id])
+    reserve = Reserve.new
+    @one_week = reserve.calc_one_week
+    @studio_reserves = reserve.reserve_list_set(params[:id])
+
+  end
+
+end
