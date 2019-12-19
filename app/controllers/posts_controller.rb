50ce2a4bc6e96ebd1cc333f202dfece0c7fe2690
yuya-yamaguchi <nsy1023n@gmail.com>
2019-12-19 13:37:37 +0900

    create posts/edit,update

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,11 +12,23 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
 
+  def edit
+    @post = Post.find(params[:id])
+  end
+
   def create
     if Post.create(post_params)
       redirect_to posts_path
     else
       render :new
+    end
+  end
+
+  def update
+    if Post.update(post_params)
+      redirect_to posts_path
+    else
+      render :edit
     end
   end
 
