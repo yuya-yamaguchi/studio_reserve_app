b2a0b878818c0e341c97a7fdd70e302f12d33d1a
yuya-yamaguchi <nsy1023n@gmail.com>
2019-12-19 11:23:23 +0900

    create posts/new

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,4 +4,20 @@ class PostsController < ApplicationController
     @posts = Post.all
   end
 
+  def new
+    @post = Post.new
+  end
+
+  def create
+    if Post.create(post_params)
+      redirect_to posts_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :contents).merge(user_id: current_user.id)
+  end
 end
