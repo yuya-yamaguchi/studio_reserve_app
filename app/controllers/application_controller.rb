1ca5f93a2447f0186b2c34526197dcf6ea3c23a8
yuya-yamaguchi <nsy1023n@gmail.com>
2019-12-31 02:09:26 +0900

    corrected header

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :studio_list
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :first_name, :last_name, :tel_no])
+  end
+
+  def studio_list
+    @studio_lists = Studio.all
+  end
 end
