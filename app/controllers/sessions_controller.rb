b24395f977417851bb2b290b012704f05b86fcc5
yuya-yamaguchi <nsy1023n@gmail.com>
2020-01-15 14:41:09 +0900

    corrected session design

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,7 @@
 class SessionsController < ApplicationController
 
   def index
-    @sessions = Session.all
+    @sessions = Session.where("date >= ?", Date.today).order(date: "DESC")
   end
 
   def show
