e0063bcc0fbc78474f88f81abd50f88c6e1b5182
yuya-yamaguchi <nsy1023n@gmail.com>
2020-01-15 16:26:40 +0900

    add entry_sessions/destroy

--- app/controllers/entry_sessions_controller.rb
+++ app/controllers/entry_sessions_controller.rb
@@ -9,4 +9,13 @@ class EntrySessionsController < ApplicationController
       render music_session_path(params[:music_session_id])
     end
   end
+
+  def destroy
+    entry_session = current_user.entry_sessions.find_by(session_id: params[:music_session_id])
+    entry_parts = current_user.entry_parts.where(session_id: params[:music_session_id])
+    
+    if entry_session.destroy && entry_parts.update(user_id: nil)
+       redirect_to music_session_path(params[:music_session_id])
+    end
+  end
 end
