7a4e1c9ace090b3b555c5e8c723bf03bbe7894fc
yuya-yamaguchi <nsy1023n@gmail.com>
2020-01-16 12:42:33 +0900

    corrected top-page design

--- app/assets/javascripts/top/index.js
+++ app/assets/javascripts/top/index.js
@@ -1,8 +1,23 @@
 $(function() {
+  // トップページの画像スライド
   $('.image-slider').slick({
     dots: true,
     autoplay: true,
     autoplaySpeed: 3000,
     speed: 800
+  });
+  // このアプリについてのスクロールリンク
+  $('#about-this-app-link').on('click', function(e){
+    var i = $('#about-this-app-link').index(this);
+    var p = $(".app-explain__title").eq(i).offset().top;
+    $('html,body').animate({ scrollTop: p - 100 }, 'fast');
+    return false;
+  });
+  // スタジオ一覧についてのスクロールリンク
+  $('#studio-list-link').on('click', function(e){
+      var i = $('#studio-list-link').index(this);
+      var p = $(".studio-list").eq(i).offset().top;
+      $('html,body').animate({ scrollTop: p - 100 }, 'fast');
+      return false;
   });
 });
