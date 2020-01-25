e4c48b7830b62116cd2a6c31e105782999192fb1
yuya-yamaguchi <nsy1023n@gmail.com>
2020-01-25 16:57:10 +0900

    add exception handling at entry_sessions

--- app/controllers/entry_sessions_controller.rb
+++ app/controllers/entry_sessions_controller.rb
@@ -1,21 +1,32 @@
 class EntrySessionsController < ApplicationController
 
   def create
-    entry_session = EntrySession.new(session_id: params[:music_session_id],
-                                     user_id: current_user.id)
-    if entry_session.save
+    begin
+      entry_session = EntrySession.new(session_id: params[:music_session_id],
+                                       user_id: current_user.id)
+      entry_session.save!
       redirect_to music_session_path(params[:music_session_id])
-    else
-      render music_session_path(params[:music_session_id])
+    rescue => e
+      error_log = ErrorLog.new
+      error_log.create_log(params, e, request.remote_ip)
+      render template: "common/error1"
     end
   end
 
   def destroy
-    entry_session = current_user.entry_sessions.find_by(session_id: params[:music_session_id])
-    entry_parts = current_user.entry_parts.where(session_id: params[:music_session_id])
-    
-    if entry_session.destroy && entry_parts.update(user_id: nil)
-       redirect_to music_session_path(params[:music_session_id])
+    begin
+      ActiveRecord::Base.transaction do
+        entry_session = current_user.entry_sessions.find_by(session_id: params[:music_session_id])
+        entry_parts = current_user.entry_parts.where(session_id: params[:music_session_id])
+
+        entry_session.destroy!
+        entry_parts.update(user_id: nil)
+      end
+      redirect_to music_session_path(params[:music_session_id])
+    rescue => e
+      error_log = ErrorLog.new
+      error_log.create_log(params, e, request.remote_ip)
+      render template: "common/error1"
     end
   end
 end
