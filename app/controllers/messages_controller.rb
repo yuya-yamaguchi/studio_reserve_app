f222adf87fd45d24a676f4c76b25998806c0bf27
yuya-yamaguchi <nsy1023n@gmail.com>
2020-01-25 15:39:03 +0900

    add exception handling at entry_parts

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,14 +4,20 @@ class MessagesController < ApplicationController
   before_action :sign_in_check, only: [:create]
 
   def create
-    @chatroom = Chatroom.find(params[:chatroom_id])
-    @message = @chatroom.messages.new(message_params)
-    
-    if @message.save && @chatroom.update(updated_at: DateTime.now)
-      respond_to do |format|
-        format.html { redirect_to chatroom_path(@chatroom) }
-        format.json { render json: { text: @message.text, created_at: @message.created_at.strftime("%Y/%m/%d %H:%M") }}
+    begin
+      @chatroom = Chatroom.find(params[:chatroom_id])
+      @message = @chatroom.messages.new(message_params)
+
+      if @message.save && @chatroom.update(updated_at: DateTime.now)
+        respond_to do |format|
+          format.html { redirect_to chatroom_path(@chatroom) }
+          format.json { render json: { text: @message.text, created_at: @message.created_at.strftime("%Y/%m/%d %H:%M") }}
+        end
       end
+    rescue => e
+      error_log = ErrorLog.new
+      error_log.create_log(params, e, request.remote_ip)
+      render template: "common/error1"
     end
   end
 
@@ -22,7 +28,7 @@ class MessagesController < ApplicationController
 
   def sign_in_check
     unless user_signed_in?
-      flash[:notice] = 'ログイン後（または会員登録後）を行ってください'
+      flash[:notice] = 'ログイン（または会員登録）を行ってください'
       redirect_to new_user_session_path
     end
   end
